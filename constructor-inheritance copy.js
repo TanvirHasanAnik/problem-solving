f612b37e8f046a2842d99fb041db8f5571a85770
Tanvir Hassan Anik <tanvir.h.anik@gmail.com>
2025-03-26 00:55:14 +0600

    inheritance

--- constructor-inheritance copy.js
+++ constructor-inheritance copy.js
@@ -3,12 +3,12 @@ function Person(name,age,height,weight){
     this.age = age;
     this.height = height;
     this.weight = weight;
-}
-Person.prototype.introduce = function(){
-    console.log(`Hello this is ${this.name} and I am ${this.age} Years Old`);
-}
-Person.prototype.bodyShape = function(){
-    console.log(`My height is ${this.height} feet weight is ${this.weight} Kg`);
+    this.introduce = function(){
+        console.log(`Hello this is ${this.name} and I am ${this.age} Years Old`);
+    }
+    this.bodyShape = function(){
+        console.log(`My height is ${this.height} feet weight is ${this.weight} Kg`);
+    }
 }
 
 let Anik = new Person('Anik',24,5.6,81);
@@ -18,13 +18,9 @@ Anik.bodyShape();
 function Athlete(sport,name,age,height,weight){
     Person.call(this,name,age,height,weight);
     this.sport = sport;
-}
-
-Athlete.prototype = Object.create(Person.prototype);
-Athlete.prototype.constructor = Athlete;
-
-Athlete.prototype.plays = function(){
-    console.log(`I play ${this.sport}`);
+    this.plays = function(){
+        console.log(`I play ${this.sport}`);
+    }
 }
 
 let Tanvir = new Athlete('football','Tanvir',24,5.6,81);
