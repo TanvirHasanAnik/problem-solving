babdc71f7057bed223d8b5e46ad6579cd37eb0b6
Anik <tanvir.h.anik@gmail.com>
2025-04-17 17:38:44 +0600

    ...

--- inventorymanager/add-product.js
+++ inventorymanager/add-product.js
@@ -1,11 +1,21 @@
 
 const form = document.getElementById("add-product-form");
 
+function product(){
+    this.id = null;
+    this.Name = null;
+    this.price = null;
+    this.size = null;
+    this.quantity = null;
+    this.date = null;
+    this.availability = null;
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formData = new FormData(form);
 
-  for (let data of formData.entries()) {
-    console.log(`${data[0]}: ${data[1]}`);
+  for (let [name,value] of formData.entries()) {
+    console.log(`${name}: ${value}`);
   }
 });
